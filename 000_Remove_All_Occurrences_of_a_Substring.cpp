f10c84ff6b29c469d9b97fce52766cce170ffb6b
manish kandari <manishkandari43@gmail.com>
2025-02-11 19:38:52 +0530

    LeetCode solution added: 000_Remove_All_Occurrences_of_a_Substring.cpp

---
+++ 000_Remove_All_Occurrences_of_a_Substring.cpp
@@ -0,0 +1,18 @@
+int m=s.size();
+        int n=part.size();
+        int i=0;//for s
+        }
+    }
+    void removeSubstrings(string& s,string& part,vector<int>& lps){
+                if(i>0)     i=lps[i-1];
+                else        j++;
+            }
+                j++;
+            }else{
+            if(part[i]==part[j]){
+                lps[j]=1+i;
+                i++;
+        int i=0,j=1;
+        while(j<n){
+class Solution {
+    void calculateLPS(string& part,int& n,vector<int>& lps){
