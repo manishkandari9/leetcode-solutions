be97eaf4c8097d08f65fde83c51b9f732b43c536
manish kandari <manishkandari43@gmail.com>
2025-02-24 19:56:22 +0530

    🚀 Auto-sync LeetCode solution: 0909_909__Snakes_and_Ladders.cpp

---
+++ 0909_909__Snakes_and_Ladders.cpp
@@ -0,0 +1,16 @@
+class Solution {
+bool findBobPath(vector<vector<int>>& adj,int bob,int parent,vector<int>& curr_path,vector<int>& bob_path){
+if(bob==0){
+bob_path = curr_path;
+return true;
+}
+//Traverse all nbrs
+curr_path.push_back(bob);
+for(int nbr: adj[bob]){
+if(nbr!=parent and findBobPath(adj,nbr,bob,curr_path,bob_path))
+return true;
+}
+curr_path.pop_back();
+return false;
+}
+int findMaxIncomeForAlice(vector<vector<int>>& adj,int alice,int parent,vector<int>& amount){
