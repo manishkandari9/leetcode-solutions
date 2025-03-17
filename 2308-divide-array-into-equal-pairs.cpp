fec4080d5c7811633f9d8c2055cd7500c9495676
manish kandari <manishkandari43@gmail.com>
2025-03-17 22:02:00 +0530

    Auto-sync LeetsyncX solution: 2308-divide-array-into-equal-pairs.cpp

---
+++ 2308-divide-array-into-equal-pairs.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool divideArray(vector<int>& nums) {
+        unordered_set<int> remaining;
+        for(int ele: nums){
+            if(remaining.count(ele))    remaining.erase(ele);
+            else                        remaining.insert(ele);
+        }
+        return remaining.size()==0;
+    }
+};
